dfe95c39b4b96de8e2381ce2ad0b42c820a8b3b5
formdenes <formdenes@gmial.com>
2019-04-03 10:16:30 +0200

    created button and display

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+function Button (props) {
+  return <button onClick={props.handleClick}>{props.label}</button>;
+}
+
+function Display (props) {
+  return (
+  <div>
+    {props.children}
+  </div>
+  );
+}
+
+
 
 ReactDOM.render(
   <h1>Hello, Green Fox Academy!</h1>,
