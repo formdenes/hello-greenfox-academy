cb529bcefcbd5dbc96abfd04e44024d140f83a1a
formdenes <formdenes@gmial.com>
2019-04-03 10:48:25 +0200

    acorn task

--- src/index.js
+++ src/index.js
@@ -2,46 +2,54 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
-function Button (props) {
-  return <button onClick={props.handleClick}>{props.label}</button>;
+function Button(props) {
+  return (<button onClick={props.handleClick}>{props.label}</button>);
 }
 
-function Display (props) {
+function Display(props) {
   return (
-  <div>
-    {props.children}
-  </div>
+    <div>
+      {props.children}
+    </div>
   );
 }
 
 class SimpleGoldenAcornApp extends React.Component {
-  state = {
-    acornAmount: 0
+  constructor() {
+    super();
+    this.state = {
+      acornAmount: 0,
+    };
+    this.increaseAcorns = this.increaseAcorns.bind(this);
+    this.decreaseAcorns = this.decreaseAcorns.bind(this);
   }
-  increaseAcorns = (e) => {
+
+  increaseAcorns(e) {
     this.setState({
-      acornAmount: this.state.acornAmount+1
-    })
+      acornAmount: this.state.acornAmount + 1,
+    });
   }
-  decreaseAcorns = (e) => {
+
+  decreaseAcorns(e) {
     this.setState({
-      acornAmount: Math.max(this.state.acornAmount-1,0)
-    })
+      acornAmount: Math.max(this.state.acornAmount - 1, 0),
+    });
   }
+
   render() {
     return (
       <div>
-        <Button label='Buy one' handleClick={this.increaseAcorns} />
+        <Button label="Buy one" handleClick={this.increaseAcorns} />
         <Display>
           <p>{this.state.acornAmount}</p>
         </Display>
-        <Button label='Eat one' handleClick={decreaseAcorns} />
+        <Button label="Eat one" handleClick={this.decreaseAcorns} />
       </div>
-    )
+    );
   }
 }
 
 ReactDOM.render(
-  <SimpleGoldenAcornApp/>,
+  <SimpleGoldenAcornApp />,
   document.getElementById('root'),
 );
