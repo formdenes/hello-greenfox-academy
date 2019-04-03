294a05b07774d29e06bca023cb68e11735eebcfd
formdenes <formdenes@gmial.com>
2019-04-03 15:10:52 +0200

    used prevState

--- src/index.js
+++ src/index.js
@@ -15,29 +15,23 @@ function Display(props) {
 }
 
 class SimpleGoldenAcornApp extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      acornAmount: 0,
-    };
-    this.increaseAcorns = this.increaseAcorns.bind(this);
-    this.decreaseAcorns = this.decreaseAcorns.bind(this);
-    this.handleKeyDown = this.handleKeyDown.bind(this);
+  state = {
+    acornAmount: 0,
   }
 
-  increaseAcorns(e) {
-    this.setState({
-      acornAmount: this.state.acornAmount + 1,
+  increaseAcorns = (e) => {
+    this.setState(prevState => { return {
+      acornAmount: prevState.acornAmount + 1};
     });
   }
 
-  decreaseAcorns(e) {
-    this.setState({
-      acornAmount: Math.max(this.state.acornAmount - 1, 0),
+  decreaseAcorns = (e) => {
+    this.setState(prevState => { return {
+      acornAmount: Math.max(prevState.acornAmount - 1, 0)};
     });
   }
 
-  handleKeyDown(e) {
+  handleKeyDown = (e) => {
     if (e.keyCode === 38) {
       this.increaseAcorns();
     } else if (e.keyCode === 40) {
@@ -50,7 +44,7 @@ class SimpleGoldenAcornApp extends React.Component {
       <div tabIndex="0" onKeyDown={this.handleKeyDown}>
         <Button label="Buy one" handleClick={this.increaseAcorns} />
         <Display>
-          <p>{this.state.acornAmount}</p>
+          <h1>{this.state.acornAmount}</h1>
         </Display>
         <Button label="Eat one" handleClick={this.decreaseAcorns} />
       </div>
